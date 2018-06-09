465405b91c36c0d01ca8cc6859c412d615d471e4
YomeR7 <33492316+YomeR7@users.noreply.github.com>
2018-06-09 13:08:18 +0200

    Update serveur.sh

--- serveur.sh
+++ serveur.sh
@@ -15,17 +15,16 @@ PORT="$1"
 
 FIFO="/tmp/$USER-fifo-$$"
 
-
 # Il faut détruire le tube quand le serveur termine pour éviter de
 # polluer /tmp.  On utilise pour cela une instruction trap pour être sur de
 # nettoyer même si le serveur est interrompu par un signal.
 function nettoyage() { rm -f "$FIFO"; }
+    
 trap nettoyage EXIT
 
 # on crée le tube nommé
 
 [ -e "$FIFO" ] || mkfifo "$FIFO"
-
 
 function accept-loop() {
     while true; do
@@ -35,48 +34,46 @@ function accept-loop() {
 
 function client() {
 local cmd serveur
-while true; do
-read cmd serveur || exit -1
-if [ $cmd = "-list" ]; then
-  for fichier in `ls $serveur`
-   do
- Vtest=`head -n 1 $serveur/$fichier | egrep '[0-9]+:[0-9]+' `
-        if [[ -n $Vtest ]]; then
-         echo "$fichier"
-        else
-         echo "$fichier n'est pas une archive" 
-        fi
-   done
-elif  [ $cmd = "-browse" ]
-then
-echo "browse"
-for((;;))
-do #boucle infinie
- read -p "vsh:>" cm
- case $cm in
-        "exit")
-                exit 0
-                ;;
-        "pwd")
-                ;;
-        "ls")
-                ;;
-        "cd")
-                ;;
-        "cat")
-                ;;
-        "rm")
-                ;;
+    while true; do
+        read cmd serveur || exit -1
+            if [ $cmd = "-list" ]; then
+                for fichier in `ls $serveur`
+                do
+                    Vtest=`head -n 1 $serveur/$fichier | egrep '[0-9]+:[0-9]+' `
+                        if [[ -n $Vtest ]]; then
+                            echo "$fichier"
+                        else
+                            echo "$fichier n'est pas une archive" 
+                        fi
+                done
+            elif  [ $cmd = "-browse" ]; then 
+                echo "browse"
+                for((;;))
+                do #boucle infinie
+                    read -p "vsh:>" cm
+                        case $cm in
+                            "exit")
+                                exit 0
+                                    ;;
+                            "pwd")
+                                    ;;
+                            "ls")
+                                    ;;
+                            "cd")
+                                    ;;
+                            "cat")
+                                    ;;
+                            "rm")
+                                    ;;
 
-esac
-done
-elif [ $cmd = "-extract" ]
-then
-echo "extract"
-else
- echo "option inconnu utilisez \"-list\" ,\"-browse\", ou \"-extract\""
-fi
-done
+                        esac
+                done
+            elif [ $cmd = "-extract" ]; then
+                echo "extract"
+            else
+                echo "option inconnu utilisez \"-list\" ,\"-browse\", ou \"-extract\""
+            fi
+    done
  }
 
 accept-loop
